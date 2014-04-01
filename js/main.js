52d47ad4e2534e134dfe82c311eb022518cd4342
Judson Mitchell <judsonmitchell@gmail.com>
2014-04-01 14:04:15 -0500

    Use toggle buttons for selections

--- js/main.js
+++ js/main.js
@@ -1,1 +1,15 @@
+$(document).ready(function (){
+    $.getJSON('data/data.json', null)
+        .done(function (data) {
+            console.log('doo');
+            $.each(data, function (index, prop){
+                console.log(index + ' ' + prop.name);
+                var item = '<button type="button" class="btn btn-default" data-toggle="button">' + prop.name + '</button>';
+                $('div.btn-group-vertical').append(item);
 
+            });
+        })
+        .fail(function (jqxhr, textStatus, error){
+            console.log(textStatus + ' ' + error);
+        });
+});
